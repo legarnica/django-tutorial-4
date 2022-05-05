b62af38248cb36740c6d3da0062d01e3d80b3b1d
Luis Herrera <elrerag@gmail.com>
2022-05-05 15:44:48 -0400

    https://docs.djangoproject.com/en/4.0/intro/tutorial04/ vistas genéricas

--- polls/views.py
+++ polls/views.py
@@ -3,6 +3,24 @@ from django.template import loader  # el import render, lo deja obsoleto.
 from polls.models import Choice, Question
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
+from django.views import generic
+
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
 
 
 def index(request):
